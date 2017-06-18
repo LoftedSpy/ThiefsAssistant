ea1a7cde40d3cc448fde9fb1bfe1f3aabaa2f782
Allen Santmier <loftedspy@gmail.com>
2017-06-18 15:30:45 -0400

    Add files via upload
    Added fence counters and timer

--- ThiefsAssistant.lua
+++ ThiefsAssistant.lua
@@ -16,10 +16,27 @@ function ThiefsAssistant.OnAddonLoad(eventType, addonName)
 
 	local window = ThiefsAssistantWindow
 
-	local MyTexture = WINDOW_MANAGER:CreateControl("ThiefsAssistantTextureTest", window, CT_TEXTURE)
-	MyTexture:SetDimensions(20, 20)
-	MyTexture:SetAnchor(TOPLEFT, window, TOPLEFT, 5, 5)
-	MyTexture:SetTexture("esoui/art/stats/justice_bounty_icon-white.dds")
+	-- Add textures
+	-- put this in a loop for god's sake
+	local texB = WINDOW_MANAGER:CreateControl("ThiefsAssistantTextureBounty", window, CT_TEXTURE)
+	texB:SetDimensions(20, 20)
+	texB:SetAnchor(TOPLEFT, window, TOPLEFT, 5, 5)
+	texB:SetTexture("esoui/art/stats/justice_bounty_icon-white.dds")
+
+	local texS = WINDOW_MANAGER:CreateControl("ThiefsAssistantTextureSells", window, CT_TEXTURE)
+	texS:SetDimensions(20, 20)
+	texS:SetAnchor(TOPLEFT, window, TOPLEFT, 130, 5)
+	texS:SetTexture("esoui/art/stats/justice_bounty_icon-red.dds")
+
+	local texL = WINDOW_MANAGER:CreateControl("ThiefsAssistantTextureLaunders", window, CT_TEXTURE)
+	texL:SetDimensions(20, 20)
+	texL:SetAnchor(TOPLEFT, window, TOPLEFT, 285, 5)
+	texL:SetTexture("esoui/art/stats/justice_bounty_icon-red.dds")
+
+	local texFT = WINDOW_MANAGER:CreateControl("ThiefsAssistantTextureFenceTimer", window, CT_TEXTURE)
+	texFT:SetDimensions(20, 20)
+	texFT:SetAnchor(TOPLEFT, window, TOPLEFT, 465, 5)
+	texFT:SetTexture("esoui/art/stats/justice_bounty_icon-white.dds")
 
 	-- The following event only triggers when bounty increases so updater is still necessary
 	-- EVENT_JUSTICE_INFAMY_UPDATED will also work
@@ -28,8 +45,21 @@ function ThiefsAssistant.OnAddonLoad(eventType, addonName)
 		time = 0
 	end)
 
-	-- Initialize bounty
+	EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_JUSTICE_FENCE_UPDATE, function()
+		local totalSells, sellsUsed = GetFenceSellTransactionInfo()
+		local totalLaunders, laundersUsed = GetFenceLaunderTransactionInfo()
+		GetControl(window, "FenceSellsValue"):SetText(totalSells - sellsUsed .. "/" .. totalSells)
+		GetControl(window, "FenceLaundersValue"):SetText(totalLaunders - laundersUsed .. "/" .. totalLaunders)
+		time = 0
+	end)
+
+	-- Initialize values
 	GetControl(window, "BountyValue"):SetText(GetFullBountyPayoffAmount())
+	local totalSells, sellsUsed, fenceResetTime = GetFenceSellTransactionInfo()
+	local totalLaunders, laundersUsed = GetFenceLaunderTransactionInfo()
+	GetControl(window, "FenceSellsValue"):SetText(totalSells - sellsUsed .. "/" .. totalSells)
+	GetControl(window, "FenceLaundersValue"):SetText(totalLaunders - laundersUsed .. "/" .. totalLaunders)
+	GetControl(window, "FenceTimerValue"):SetText(fenceResetTime)
 
 	local saveData = ZO_SavedVars:NewAccountWide("ThiefsAssistantSavedVars", 1)
 	ThiefsAssistant.savedVars = saveData
@@ -50,11 +80,26 @@ function ThiefsAssistant.OnAddonLoad(eventType, addonName)
 	end)
 end
 
+local function secondsToTime(original)
+	seconds = math.floor(original % 60)
+	hours = math.floor(original / 3600)
+	minutes = math.floor(original / 60) - (60 * hours)
+	
+	result = hours .. ":" .. minutes .. ":" .. seconds
+	return result
+end
+
 function ThiefsAssistantOnUpdate()
 	time = time + 1
-	-- exectues every updateTimer seconds
+	-- Every UPDATE_TIMER frames update values
 	if(time > UPDATE_TIMER) then
 		GetControl(ThiefsAssistantWindow, "BountyValue"):SetText(GetFullBountyPayoffAmount())
+		time = 0
+	end
+	-- Every 30 frames update timers
+	if(time % 30 == 0) then
+		local a, b, timeSeconds = GetFenceSellTransactionInfo()
+		GetControl(ThiefsAssistantWindow, "FenceTimerValue"):SetText(secondsToTime(timeSeconds))
 		time = 0
 	end
 end
